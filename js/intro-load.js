6f67874630122e80c752d187bbf18dabcc394008
Felix Rafael De Guzman <deguzman.felixrafael@auf.edu.ph>
2023-11-09 04:54:18 +0800

    test commit

--- js/intro-load.js
+++ js/intro-load.js
@@ -22,53 +22,47 @@ gsap.fromTo(
   }
 );
 
-var cursor = document.querySelector('.cursor');
-var cursorinner = document.querySelector('.cursor2');
-var a = document.querySelectorAll('a');
+const cursor = document.querySelector('.cursor');
+const cursorinner = document.querySelector('.cursor2');
+const a = document.querySelectorAll('a');
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
+function updateCursorPos(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`;
+    cursorinner.style.left = x + 'px';
+    cursorinner.style.top = y + 'px';
+}
 
-  localStorage.setItem('cursorX', x);
-  localStorage.setItem('cursorY', y);
+document.addEventListener('mousemove', updateCursorPos);
 
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+document.addEventListener('mousedown', function () {
+    cursor.classList.add('click');
+    cursorinner.classList.add('cursorinnerhover');
 });
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursorinner.style.left = x + 'px';
-  cursorinner.style.top = y + 'px';
-});
-
-document.addEventListener('mousedown', function(){
-  cursor.classList.add('click');
-  cursorinner.classList.add('cursorinnerhover')
-});
-
-document.addEventListener('mouseup', function(){
-  cursor.classList.remove('click')
-  cursorinner.classList.remove('cursorinnerhover')
+document.addEventListener('mouseup', function () {
+    cursor.classList.remove('click');
+    cursorinner.classList.remove('cursorinnerhover');
 });
 
 a.forEach(item => {
-  item.addEventListener('mouseover', () => {
-    cursor.classList.add('hover');
-  });
-  item.addEventListener('mouseleave', () => {
-    cursor.classList.remove('hover');
-  });
-})
+    item.addEventListener('mouseover', () => {
+        cursor.classList.add('hover');
+    });
+    item.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
+    });
+});
 
+// Check if the device is a mobile device
 function isMobileDevice() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-// Hide the cursor on mobile devices
 if (isMobileDevice()) {
-  cursor.style.display = "none";
+    cursor.style.display = "none";
+    cursorinner.style.display = "none";
 }
 
 function showCursor() {
